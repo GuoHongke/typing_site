6e36500fe194a0e9c4366659103c91a5df48626f
郭红科 <guohongke@guohongkedeMacBook-Pro.local>
2017-08-29 16:29:22 +0800

    数据库的初步对接

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from src.utils import config
+
+DB_HOST = config.get('mysql', 'host')
+DB_PORT = config.get('mysql', 'port')
+DB = config.get('mysql', 'database')
+DB_USER = config.get('mysql', 'user')
+DB_PWD = config.get('mysql', 'password')
+mysql_engine = 'mysql+mysqldb://%s:%s@%s:%s/%s' % (DB_USER, DB_PWD, DB_HOST, DB_PORT, DB)
+
+Base = declarative_base()
+
+engine = create_engine(mysql_engine)
+DBSession = sessionmaker(bind=engine)
